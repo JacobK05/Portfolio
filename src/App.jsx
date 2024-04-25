3e0a74d08c7636ffa267a870623fc71daf809441
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-04-25 15:43:03 -0500

    got links to display content

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import Nav from './components/nav'
-import Body from './components/body'
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/nav';
+import Body from './components/body';
+import About from './pages/about';
+import Contact from './pages/contact';
+import './App.css';
 
 function App() {
-  return (
-    <>
-    <BrowserRouter>
-    <Nav />
-    <Body />
-    
-    </BrowserRouter>
-    
-    
-    </>
-  )
-  
-  
+    return (
+        <>
+            <BrowserRouter>
+                <Nav />
+                <Routes>
+                    <Route exact path="/" element={<Body />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,6 +6,7 @@ export default function nav () {
     return(
         <Navbar
         links={[
+          <Link key={1} className="link" to="/">Home</Link>,
           <Link key={2} className="link" to="/about">About</Link>,
           <Link key={3} className="link" to="/contact">Contact</Link>,
           <Link key={4} className="link" to="/resume">Resume</Link>,
