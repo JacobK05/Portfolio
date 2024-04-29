65a5aa9d6991b572403d8731cd393c4e8269bfc3
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-04-29 12:34:51 -0500

    added and contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,46 @@
-export default function contact() {
-    return 
-       
-        
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-    
-}
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_f9cx308",
+        "template_vay24te",
+        form.current,
+        "97IPuAn58TnfZVWFU"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <>
+      <form className='contact-form' ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input placeholder="Name:" type="text" name="user_name" />
+        <label>Email</label>
+        <input placeholder="Email:" type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input className="input-btn" type="submit" value="Send" />
+      </form>
+    </>
+  );
+};
+
+export default Contact;
+
+// Styles
