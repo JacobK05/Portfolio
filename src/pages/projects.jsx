09fe75a550b80d781d05300d1d69b9637c1b26ed
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-05-27 12:05:44 -0500

    fixing image issue when deployed

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,28 +1,29 @@
-import projects from "../projects";
+//import projects from "../projects";
 import './pages.css'
+import PasswordGen from '/assets/PasswordGen.png'
 export default function PortfolioPage() {
   return (
     <div className="container" >
       <h3 className="contactH3">Projects ... Click on the image</h3>
       <div className="row">
-        {projects.map((project, index) => (
-          <div key={index} className="card col-md-6 col-lg-4 col-xl-3">
+        
+          <div className="card col-md-6 col-lg-4 col-xl-3">
             <p>
-              <a href={project.deployed}>
+              <a href='https://jacobk05.github.io/java-challenge/'>
                 <img
                   className="images"
-                  src={project.image}
+                  src={PasswordGen}
                   alt="Image goes here"
                 />
               </a>
             </p>
             <div className="text-block">
-              <a className="project-title" href={project.link}> 
-              <h4 className="project-h4">{project.name}</h4>
+              <a className="project-title" href='https://github.com/JacobK05/java-challenge'> 
+              <h4 className="project-h4" style={{cursor: 'pointer'}}> Password Generator </h4>
               </a>
             </div>
           </div>
-        ))}
+    
       </div>
     </div>
   );
