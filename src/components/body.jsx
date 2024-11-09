a6b9e4aa59d3cc3034889d6341a6d8fc5fc68c3f
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-11-09 15:45:53 -0600

    changes job title

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -6,7 +6,7 @@ export default function body () {
         <a style={{textDecoration: 'none'}} href="https://docs.google.com/document/d/1PC6XDLcmLhMYdFRpZWGeKYDKGPiz3NgXeFBiRe1GWhs/edit?usp=sharing"> 
         <h1 className='name' > Jacob Kaufenberg </h1>
         </a>
-        <p className='name-text'> <a className='job-text'> Junior </a> <a className='job-text'> Software </a> <a className='job-text'> Engineer </a></p>
+        <p className='name-text'><a className='job-text'> Cybersecurity </a></p>
     </div>   
 
     </>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,11 +4,7 @@ export default function about() {
  return(
    <div className="about-me">
    <h1 className="about-h1"> About Me </h1>
-   <p className="about-p">My name is Jacob Kaufenberg, a motivated 19-year-old high school graduate with a passion for the tech industry. I'm driven to carve out a successful career as a software developer, where I can continually grow and refine my skills. With a solid foundation in programming and a keen eye for detail, I'm eager to tackle challenges head-on and contribute meaningfully to innovative projects.
-
-    Outside of programming, I bring to the table a strong sense of discipline, consistency, and determination. These qualities not only fuel my personal growth but also drive me to excel in every aspect of my professional endeavors.
-
-   While I may not yet possess all the experience or expertise for every role, I am committed to learning and adapting to become the best possible candidate. I believe that with perseverance and dedication, I can contribute significantly to any team and achieve the goals I've set for myself in the dynamic world of technology.         </p>
+   <p className="about-p">Highly motivated 19-year-old high school graduate with a passion for the tech industry. Dedicated to pursuing a successful career in IT, leveraging strong technical skills and a problem-solving mindset to address challenges and contribute to innovative solutions. Possesses a solid foundation in information technology and a commitment to continuous learning and growth. Known for discipline, consistency, and determination, driving personal and professional success. Committed to adapting and excelling in the ever-evolving field of technology.</p>
 </div>
  )
 }
